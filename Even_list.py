862d35295c2af0405d427502a6e7a2e03b412490
asha <asharani17.n@gmail.com>
2024-11-15 12:19:53 -0500

    adding even list finder code

---
+++ Even_list.py
@@ -0,0 +1,16 @@
+a_input = input('Enter the numbers seperated by comma:')
+a_list = a_input.split(',')
+
+
+def even_list_finder(a:list) -> list[int]:
+    output_list=[]
+    for num in a:
+        if int(num) % 2 == 0:
+            output_list.append(int(num))
+    
+    return output_list
+
+
+print(even_list_finder(a_list))
+
+
